ab6170cedec07a6d7554916c859d36329f1a4125
xloem <0xloem@gmail.com>
2022-02-02 10:16:24 +0000

    feature: calc_fn and sparse broadcasting for bias, mask, weights
    rebased from 66706d510f78dfff682aa041a5614165de4d5c06

--- memory_efficient_attention/attention_torch.py
+++ memory_efficient_attention/attention_torch.py
@@ -4,23 +4,34 @@ from .utils import dynamic_slice, map_pt, scan
 import math
 
 
-def _query_chunk_attention(query, key, value, mask, bias, key_chunk_size=4096):
+def _query_chunk_attention(query_idx, query, key, value,
+                           mask, bias, key_chunk_size=4096,
+                           mask_calc_fn=None,
+                           bias_calc_fn=None,
+                           weights_calc_fn=None,
+                           calc_fn_data=None):
     num_kv, num_heads, k_features = key.shape[-3:]
     v_features = value.shape[-1]
     num_q = query.shape[-3]
     key_chunk_size = min(key_chunk_size, num_kv)
     query = query / math.sqrt(k_features)
 
-    def summarize_chunk(query, key, value, mask, bias):
+    def summarize_chunk(key_idx, query, key, value, mask, bias):
         attn_weights = torch.einsum('...qhd,...khd->...qhk', query, key)
+        if bias_calc_fn is not None:
+            bias = bias_calc_fn(query_idx, key_idx, bias, attn_weights, calc_fn_data)
         if bias is not None:
             bias = torch.einsum('...hqk->...qhk', bias)
             attn_weights = attn_weights + bias
+        if mask_calc_fn is not None:
+            mask = mask_calc_fn(query_idx, key_idx, mask, attn_weights, calc_fn_data)
         if mask is not None:
             big_neg = torch.finfo(attn_weights.dtype).min
             big_neg = torch.tensor(big_neg, dtype=torch.float32)
             mask = torch.einsum('...hqk->...qhk', mask)
             attn_weights = torch.where(mask, attn_weights, big_neg)
+        if weights_calc_fn is not None:
+            attn_weights = weights_calc_fn(query_idx, key_idx, attn_weights, calc_fn_data)
         max_score, _ = torch.max(attn_weights, -1, keepdim=True)
         max_score = max_score.detach()
         exp_weights = torch.exp(attn_weights - max_score)
@@ -34,16 +45,22 @@ def _query_chunk_attention(query, key, value, mask, bias, key_chunk_size=4096):
         value_chunk = dynamic_slice(value, tuple([0] * (value.ndim - 3)) + (chunk_idx, 0, 0),
                                     tuple(value.shape[:-3]) + (key_chunk_size, num_heads, v_features))
         if bias is not None:
-            bias_chunk = dynamic_slice(bias, tuple([0] * (bias.ndim - 3)) + (0, 0, chunk_idx),
-                                       tuple(bias.shape[:-3]) + (num_heads, num_q, key_chunk_size))
+            if bias.shape[-1] > 1:
+                bias_chunk = dynamic_slice(bias, tuple([0] * (bias.ndim - 3)) + (0, 0, chunk_idx),
+                                           tuple(bias.shape[:-3]) + (bias.shape[-3], bias.shape[-2], key_chunk_size))
+            else:
+                bias_chunk = bias
         else:
             bias_chunk = None
         if mask is not None:
-            mask_chunk = dynamic_slice(mask, tuple([0] * (mask.ndim - 3)) + (0, 0, chunk_idx),
-                                       tuple(mask.shape[:-3]) + (num_heads, num_q, key_chunk_size))
+            if mask.shape[-1] > 1:
+                mask_chunk = dynamic_slice(mask, tuple([0] * (mask.ndim - 3)) + (0, 0, chunk_idx),
+                                           tuple(mask.shape[:-3]) + (mask.shape[-3], mask.shape[-2], key_chunk_size))
+            else:
+                mask_chunk = mask
         else:
             mask_chunk = None
-        return checkpoint(summarize_chunk, query, key_chunk, value_chunk, mask_chunk, bias_chunk)
+        return checkpoint(summarize_chunk, chunk_idx, query, key_chunk, value_chunk, mask_chunk, bias_chunk)
 
     chunk_values, chunk_weights, chunk_max = map_pt(
         chunk_scanner, xs=torch.arange(0, num_kv, key_chunk_size))
@@ -61,7 +78,11 @@ def _query_chunk_attention(query, key, value, mask, bias, key_chunk_size=4096):
 def efficient_dot_product_attention(query, key, value,
                                     mask=None, bias=None,
                                     query_chunk_size=1024,
-                                    key_chunk_size=4096):
+                                    key_chunk_size=4096,
+                                    bias_calc_fn=None,
+                                    mask_calc_fn=None,
+                                    weights_calc_fn=None,
+                                    calc_fn_data=None):
     """Computes efficient dot-product attention given query, key, and value.
       This is efficient version of attention presented in
       https://arxiv.org/abs/2112.05682v2 which comes with O(sqrt(n)) memory requirements.
@@ -75,15 +96,31 @@ def efficient_dot_product_attention(query, key, value,
           `[batch..., kv_length, num_heads, v_depth_per_head]`.
         bias: bias for the attention weights. This should be broadcastable to the
           shape `[batch..., num_heads, q_length, kv_length]`.
-          This can be used for incorporating causal masks, padding masks,
-          proximity bias, etc.
+          This can be used for incorporating padding masks, proximity bias, etc.
         mask: mask for the attention weights. This should be broadcastable to the
           shape `[batch..., num_heads, q_length, kv_length]`.
-          This can be used for incorporating causal masks.
           Attention weights are masked out if their corresponding mask value
           is `False`.
         query_chunk_size: int: query chunks size
         key_chunk_size: int: key chunks size
+        bias_calc_fn: a bias calculation callback for each chunk, of form
+          `(q_offset, k_offset, bias_chunk, attn_weights, calc_fn_data) -> bias`.
+          This can be used for incorporating causal masks, padding masks,
+          proximity bias, etc.
+        mask_calc_fn: a mask calculation callback for each chunk, of form
+          `(q_offset, k_offset, mask_chunk, attn_weights, calc_fn_data) -> mask`.
+          This can be used for incorporating causal or other large masks.
+          Attention weights are masked out if their corresponding mask value
+          is `False`.
+        weights_calc_fn: a general attn_weights callback for each chunk, of form
+          `(q_offset, k_offset, attn_weights, calc_fn_data) -> attn_weights`.
+          attn_weights has shape of
+          `[batch..., q_chunk_size, num_heads, k_chunk_size]`.
+          This can be used to implement complex weights processing in a memory
+          efficient way.
+        calc_fn_data: optional pure data to pass to each per-chunk call of
+          bias_calc_fn, mask_calc_fn, and weights_calc_fn.
+        weights_calc_data: pure_data to pass with each call to weights_calc_fn
       Returns:
         Output of shape `[batch..., q_length, num_heads, v_depth_per_head]`.
       """
@@ -95,17 +132,25 @@ def efficient_dot_product_attention(query, key, value,
         query_chunk = dynamic_slice(query, tuple([0] * (query.ndim - 3)) + (chunk_idx, 0, 0),
                                     tuple(query.shape[:-3]) + (min(query_chunk_size, num_q), num_heads, q_features))
         if mask is not None:
-            mask_chunk = dynamic_slice(mask, tuple([0] * (mask.ndim - 3)) + (0, chunk_idx, 0),
-                                       tuple(mask.shape[:-3]) + (num_heads, min(query_chunk_size, num_q), num_kv))
+            if mask.shape[-2] > 1:
+                mask_chunk = dynamic_slice(mask, tuple([0] * (mask.ndim - 3)) + (0, chunk_idx, 0),
+                                           tuple(mask.shape[:-3]) + (mask.shape[-3], min(query_chunk_size, num_q), mask.shape[-1]))
+            else:
+                mask_chunk = mask
         else:
             mask_chunk = None
         if bias is not None:
-            bias_chunk = dynamic_slice(bias, tuple([0] * (bias.ndim - 3)) + (0, chunk_idx, 0),
-                                       tuple(bias.shape[:-3]) + (num_heads, min(query_chunk_size, num_q), num_kv))
+            if bias.shape[-2] > 1:
+                bias_chunk = dynamic_slice(bias, tuple([0] * (bias.ndim - 3)) + (0, chunk_idx, 0),
+                                           tuple(bias.shape[:-3]) + (bias.shape[-3], min(query_chunk_size, num_q), bias.shape[-1]))
+            else:
+                bias_chunk = bias
         else:
             bias_chunk = None
         return (chunk_idx + query_chunk_size,
-                _query_chunk_attention(query_chunk, key, value, mask_chunk, bias_chunk, key_chunk_size=key_chunk_size))
+                _query_chunk_attention(chunk_idx, query_chunk, key, value, mask_chunk, bias_chunk, key_chunk_size=key_chunk_size,
+                                       bias_calc_fn=bias_calc_fn, mask_calc_fn=mask_calc_fn,
+                                       weights_calc_fn=weights_calc_fn, calc_fn_data=calc_fn_data))
 
     _, res = scan(chunk_scanner, init=0, xs=None, length=math.ceil(num_q / query_chunk_size))
     rl = [res[i] for i in range(res.shape[0])]
--- test/test_computation.py
+++ test/test_computation.py
@@ -1,61 +1,164 @@
 import unittest
 import torch
-import numpy as np
+import jax, jax.numpy as jnp
 from memory_efficient_attention import efficient_dot_product_attention_pt, efficient_dot_product_attention_jax
 from flax.linen.attention import dot_product_attention
+from memory_efficient_attention.utils import dynamic_slice
 
+efficient_dot_product_attention_jax = jax.jit(efficient_dot_product_attention_jax, static_argnames=('mask_calc_fn', 'bias_calc_fn', 'weights_calc_fn'))
 
 class ComputationTest(unittest.TestCase):
     @staticmethod
     def data():
         b = 8
-        Qb = np.random.rand(1, b, 128, 16, 8).astype("float32")
-        Kb = np.random.rand(1, b, 128, 16, 8).astype("float32")
-        Vb = np.random.rand(1, b, 128, 16, 8).astype("float32")
-        Mb = np.random.rand(1, b, 16, 128, 128) > 0.5
-        Bb = np.random.rand(1, b, 16, 128, 128).astype("float32") / 100
-        return Qb, Kb, Vb, Mb, Bb
+        key = jax.random.PRNGKey(0)
+        Qb = jax.random.uniform(key, (1, b, 128, 16, 8), dtype=jnp.float32)
+        Kb = jax.random.uniform(key, (1, b, 128, 16, 8), dtype=jnp.float32)
+        Vb = jax.random.uniform(key, (1, b, 128, 16, 8), dtype=jnp.float32)
+        Mb = jax.random.uniform(key, (1, b, 16, 128, 128)) > 0.5
+        Bb = jax.random.uniform(key, (1, b, 16, 128, 128), dtype=jnp.float32) / 100
+
+        # calc_fn bias & mask
+        def mask_calc_fn_jax(query_offset, key_offset, mask_chunk, attn_weights, MbBb):
+            if MbBb is not None:
+                Mb, Bb = MbBb
+                return jax.lax.dynamic_slice(Mb, tuple([0] * (Mb.ndim - 2)) + (query_offset, key_offset),
+                    slice_sizes=tuple(Mb.shape[:-2]) + (attn_weights.shape[-3], attn_weights.shape[-1]))
+            else:
+                return mask_chunk
+        def bias_calc_fn_jax(query_offset, key_offset, bias_chunk, attn_weights, MbBb):
+            if MbBb is not None:
+                Mb, Bb = MbBb
+                return jax.lax.dynamic_slice(Bb, tuple([0] * (Bb.ndim - 2)) + (query_offset, key_offset),
+                    slice_sizes=tuple(Bb.shape[:-2]) + (attn_weights.shape[-3], attn_weights.shape[-1]))
+            else:
+                return bias_chunk
+        def weights_calc_fn_jax(query_offset, key_offset, attn_weights, MbBb):
+            if MbBb is not None:
+                bias = bias_calc_fn_jax(query_offset, key_offset, None, attn_weights, MbBb)
+                bias = jnp.einsum('...hqk->...qhk', bias)
+                attn_weights = attn_weights + bias
+
+                mask = mask_calc_fn_jax(query_offset, key_offset, None, attn_weights, MbBb)
+                big_neg = jnp.finfo(attn_weights.dtype).min
+                mask = jnp.einsum('...hqk->...qhk', mask)
+                attn_weights = jnp.where(mask, attn_weights, big_neg)
+
+            return attn_weights
+        def mask_calc_fn_pt(query_offset, key_offset, mask_chunk, attn_weights, MbBb):
+            if MbBb is not None:
+                Mb, Bb = MbBb
+                return dynamic_slice(torch.tensor(Mb.to_py()), tuple([0] * (Mb.ndim - 2)) + (query_offset, key_offset),
+                    tuple(Mb.shape[:-2]) + (attn_weights.shape[-3], attn_weights.shape[-1]))
+            else:
+                return mask_chunk
+        def bias_calc_fn_pt(query_offset, key_offset, bias_chunk, attn_weights, MbBb):
+            if MbBb is not None:
+                Mb, Bb = MbBb
+                return dynamic_slice(torch.tensor(Bb.to_py()), tuple([0] * (Bb.ndim - 2)) + (query_offset, key_offset),
+                    tuple(Bb.shape[:-2]) + (attn_weights.shape[-3], attn_weights.shape[-1]))
+            else:
+                return bias_chunk
+        def weights_calc_fn_pt(query_offset, key_offset, attn_weights, MbBb):
+            if MbBb is not None:
+                bias = bias_calc_fn_pt(query_offset, key_offset, None, attn_weights, MbBb)
+                bias = torch.einsum('...hqk->...qhk', bias)
+                attn_weights = attn_weights + bias
+
+                mask = mask_calc_fn_pt(query_offset, key_offset, None, attn_weights, MbBb)
+                big_neg = torch.finfo(attn_weights.dtype).min
+                big_neg = torch.tensor(big_neg, dtype=torch.float32)
+                mask = torch.einsum('...hqk->...qhk', mask)
+                attn_weights = torch.where(mask, attn_weights, big_neg)
+            return attn_weights
+
+        return [
+            ('broadcasting simple mask and bias',
+             (Qb, Kb, Vb, Mb[:,:,:1,:1,:1], Bb[:,:,:1,:1,:1], dict(), None)),
+            ('full mask and bias, negates memory savings',
+             (Qb, Kb, Vb, Mb, Bb, dict(), None)),
+            ('broadcasting mask and bias passed through callbacks',
+             (Qb, Kb, Vb, Mb[:,:,:1,:1,:1], Bb[:,:,:1,:1,:1], dict(
+                pt_mask=mask_calc_fn_pt,
+                pt_bias=bias_calc_fn_pt,
+                pt_weights=weights_calc_fn_pt,
+                jax_mask=mask_calc_fn_jax,
+                jax_bias=bias_calc_fn_jax,
+                jax_weights=weights_calc_fn_jax,
+             ), None)),
+            ('mask and bias generated per-chunk by callbacks',
+             (Qb, Kb, Vb, Mb, Bb, dict(
+                pt_mask=mask_calc_fn_pt,
+                pt_bias=bias_calc_fn_pt,
+                jax_mask=mask_calc_fn_jax,
+                jax_bias=bias_calc_fn_jax,
+              ), (Mb, Bb))),
+            ('mask and bias manually applied by custom callback',
+             (Qb, Kb, Vb, Mb, Bb, dict(
+                pt_weights=weights_calc_fn_pt,
+                jax_weights=weights_calc_fn_jax,
+             ), (Mb, Bb))),
+            ('no mask nor bias',
+             (Qb, Kb, Vb, None, None, dict(), None)),
+        ]
 
     @staticmethod
     def calc_pt(data):
-        Qb, Kb, Vb, Mb, Bb = data
-        Qbt = torch.tensor(Qb, requires_grad=True)
-        Kbt = torch.tensor(Kb, requires_grad=True)
-        Vbt = torch.tensor(Vb, requires_grad=True)
-        Bbt = torch.tensor(Bb, requires_grad=True)
-        Mbt = torch.tensor(Mb)
-        return efficient_dot_product_attention_pt(Qbt, Kbt, Vbt, Mbt, Bbt).detach().numpy()
+        Qb, Kb, Vb, Mb, Bb, Cf, Cb = data
+        Qbt = torch.tensor(Qb.to_py(), requires_grad=True)
+        Kbt = torch.tensor(Kb.to_py(), requires_grad=True)
+        Vbt = torch.tensor(Vb.to_py(), requires_grad=True)
+        if Mb is not None and Cb is None:
+            Mbt = torch.tensor(Mb.to_py(), requires_grad=False)
+        else:
+            Mbt = None
+        if Bb is not None and Cb is None:
+            Bbt = torch.tensor(Bb.to_py(), requires_grad=False)
+        else:
+            Bbt = None
+        Mf, Bf, Wf = Cf.get('pt_mask'), Cf.get('pt_bias'), Cf.get('pt_weights')
+        return efficient_dot_product_attention_pt(Qbt, Kbt, Vbt, Mbt, Bbt,
+                                                  mask_calc_fn=Mf, bias_calc_fn=Bf,
+                                                  weights_calc_fn=Wf, calc_fn_data=Cb).detach().numpy()
 
     @staticmethod
     def calc_jax(data):
-        Qb, Kb, Vb, Mb, Bb = data
-        return np.asarray(efficient_dot_product_attention_jax(Qb, Kb, Vb, Mb, Bb))
+        Qb, Kb, Vb, Mb, Bb, Cf, Cb = data
+        Mf, Bf, Wf = Cf.get('jax_mask'), Cf.get('jax_bias'), Cf.get('jax_weights')
+        if Cb is not None:
+            Mb, Bb = None, None
+        return jnp.asarray(efficient_dot_product_attention_jax(Qb, Kb, Vb, Mb, Bb,
+                                                               mask_calc_fn=Mf, bias_calc_fn=Bf,
+                                                               weights_calc_fn=Wf, calc_fn_data=Cb))
 
     @staticmethod
     def calc_flax(data):
-        Qb, Kb, Vb, Mb, Bb = data
-        return np.asarray(dot_product_attention(Qb, Kb, Vb, Bb, Mb))
+        Qb, Kb, Vb, Mb, Bb, Cf, Pd = data
+        return jnp.asarray(dot_product_attention(Qb, Kb, Vb, Bb, Mb))
 
     def test_pt(self):
-        data = ComputationTest.data()
-        res_pt = ComputationTest.calc_pt(data)
-        res_flax = ComputationTest.calc_flax(data)
-        self.assertTrue(np.allclose(res_pt, res_flax))
+        for msg, data in ComputationTest.data():
+            with self.subTest(msg=msg):
+                res_pt = ComputationTest.calc_pt(data)
+                res_flax = ComputationTest.calc_flax(data)
+                self.assertTrue(jnp.allclose(res_pt, res_flax))
 
     def test_jax(self):
-        data = ComputationTest.data()
-        res_jax = ComputationTest.calc_jax(data)
-        res_flax = ComputationTest.calc_flax(data)
-        self.assertTrue(np.allclose(res_jax, res_flax))
+        for msg, data in ComputationTest.data():
+            with self.subTest(msg=msg):
+                res_jax = ComputationTest.calc_jax(data)
+                res_flax = ComputationTest.calc_flax(data)
+                self.assertTrue(jnp.allclose(res_jax, res_flax))
 
     def test_jax_and_pt(self):
-        data = ComputationTest.data()
-        res_pt = ComputationTest.calc_pt(data)
-        res_jax = ComputationTest.calc_jax(data)
-        res_flax = ComputationTest.calc_flax(data)
-        self.assertTrue(np.allclose(res_pt, res_jax))
-        self.assertTrue(np.allclose(res_pt, res_flax))
-        self.assertTrue(np.allclose(res_jax, res_flax))
+        for msg, data in ComputationTest.data():
+            with self.subTest(msg=msg):
+                res_pt = ComputationTest.calc_pt(data)
+                res_jax = ComputationTest.calc_jax(data)
+                res_flax = ComputationTest.calc_flax(data)
+                self.assertTrue(jnp.allclose(res_pt, res_jax))
+                self.assertTrue(jnp.allclose(res_pt, res_flax))
+                self.assertTrue(jnp.allclose(res_jax, res_flax))
 
 
 if __name__ == '__main__':
